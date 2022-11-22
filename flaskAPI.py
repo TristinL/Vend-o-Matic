ef01c5a21f7053bbc156df0a8f28e66cea2f18e2
tristinl <triplehman64@gmail.com>
2022-11-22 17:55:57 -0600

    updated address of host

--- flaskAPI.py
+++ flaskAPI.py
@@ -76,4 +76,4 @@ api.add_resource(Specific_inventory, "/inventory/<int:item_id>")
 
 #run the thing!
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5001)
+    app.run(host='0.0.0.0', port=5001)
